22440659cb3a22b936ebe1c96700680f563305b0
satya k <satyanarayna121@gmail.com>
2023-03-11 15:08:21 +0530

    first

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ const iconElem = document.querySelector('#icon');
 const tempElem = document.querySelector('#temp');
 const weatherElem = document.querySelector('#weather');
 
-document.querySelector('#getWeather').addEventListener('click',getWeather);
+document.querySelector('#getWeather').addEventListener('click',get5daysWeather);
 
 function getWeather(){
     
@@ -30,3 +30,26 @@ function getWeather(){
     locationElem.value = '';
 }
 
+function get5daysWeather(){
+    const apikey = '3af4b4ad773af5617d9f81ed2b9e54ab';
+    const loc = locationElem.value;
+    let coordinates =[] ;
+    let url = `http://api.openweathermap.org/geo/1.0/direct?q=${loc}&appid=${apikey}`
+    fetch(url).then(res=>res.json())
+    .then(data=>{
+        console.log(coordinates = data);
+    })
+    .catch(error => console.log(error))
+    
+    console.log(coordinates);
+    url = `http://api.openweathermap.org/data/2.5/forecast/daily?lat=17.360589&lon=78.4740613&cnt=5&appid=${apikey}`
+
+    fetch(url).then(res => res.json)
+    .then(data => {
+        console.log(data);
+    }).catch(error=> console.log(error))
+
+
+
+    locationElem.value = '';
+}
